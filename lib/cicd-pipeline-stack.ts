837ace483a41d994ce728825ef93207733e52801
Mike Back <hello@mikeback.me>
2023-04-22 14:29:21 +1000

    add pipeline code

--- lib/cicd-pipeline-stack.ts
+++ lib/cicd-pipeline-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 
 export class CicdPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
+    // AWS CI-CD Pipeline
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CicdPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cicdPipeline = new CodePipeline(this, "cdk-tutorial-cicd-pipeline", {
+      synth: new ShellStep("Synth", {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub("MakeItBack/cicd-pipeline", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
